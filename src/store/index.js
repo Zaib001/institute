b0e67fa9cfc5ea0a543a583ef8bc4255ce0c2c2f
TalhaZaheer1 <tzaheer72@gmail.com>
2025-07-01 08:49:49 -0700

    change

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth/authSlice'; // Replace with your path
+import authReducer from './auth/authSlice';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  enhancers: (getDefaultEnhancers) =>
+    getDefaultEnhancers().concat(composeWithDevTools()),
 });
 
 export default store;
